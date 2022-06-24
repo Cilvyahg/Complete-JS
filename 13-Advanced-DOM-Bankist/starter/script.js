f2ffd074f36749ab374d95c9f086971d73d5c6f1
Cilvya <cilvya.hg@gmail.com>
2022-06-24 15:55:07 +0200

    bankist

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,39 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//
+console.log(document.documentElement);
+console.log(document.body);
+console.log(document.head);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//creating and inserting elements
+
+const message = document.createElement('div'); // now it's not yet on the page
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+//in innerHtml you can also add elements
+message.innertHTML =
+  'We use cookies for improved functionality and analytics <button class="btn btn--close-cookie"> Got it! </button>';
+
+// prepend adds the element as the first child of the header element
+header.prepend(message); 
+
+// header.append(message); // it will be the last element of the element.
+
+header.append(message.cloneNode(true)); // clone it 
+
+header.before(message); // this element will be placed before the header element
+header.after(message); // the message element will be places after the header element
+
+
